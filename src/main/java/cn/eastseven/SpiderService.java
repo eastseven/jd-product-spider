32aad5e5d61f7477aa18c415660a1f438178a7eb
eastseven <eastseven@foxmail.com>
2017-04-10 10:07:16 +0800

    增加category抓取链接

--- src/main/java/cn/eastseven/SpiderService.java
+++ src/main/java/cn/eastseven/SpiderService.java
@@ -3,6 +3,7 @@ package cn.eastseven;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -21,18 +22,22 @@ import java.util.stream.Collectors;
 @Service
 public class SpiderService implements PageProcessor, Pipeline {
 
-    private Site site = Site.me().setDomain("jd.com");
+    private Site site = Site.me()
+            .setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36");
 
     @Autowired
     ProductRepository productRepository;
 
     final long time = 60 * 60 * 1000L;
 
+    final String special = "http://www.jd.com/special.aspx?id=5";
+    final String allSort = "https://www.jd.com/allSort.aspx";
+
     @Scheduled(fixedRate = time)
     public void start() {
         log.info("{}, Scheduled(fixedRate = {})", "spider start", time);
         Spider.create(this)
-                .addUrl("http://www.jd.com/special.aspx?id=5")
+                .addUrl(allSort, special)
                 .addPipeline(new ConsolePipeline())
                 .addPipeline(this)
                 .thread(Runtime.getRuntime().availableProcessors())
@@ -41,6 +46,39 @@ public class SpiderService implements PageProcessor, Pipeline {
 
     @Override
     public void process(Page page) {
+        log.info("1={}", page.getUrl());
+        if (special.equals(page.getUrl())) {
+            log.info("2={}", page.getUrl());
+            processProduct(page);
+        } else if (allSort.equals(page.getUrl())) {
+            log.info("3={}", page.getUrl());
+            processCategory(page);
+        }
+    }
+
+    @Override
+    public Site getSite() {
+        return site;
+    }
+
+    @Override
+    public void process(ResultItems resultItems, Task task) {
+        if (resultItems.getAll().isEmpty()) return;
+
+        try {
+            List<Product> products = resultItems.get("products");
+            List<Product> needSave = products.stream()
+                    .filter(product -> !productRepository.exists(product.getId()))
+                    .collect(Collectors.toList());
+
+            productRepository.save(needSave);
+            log.info("save {}, total {}", needSave.size(), productRepository.count());
+        } catch (Exception e) {
+            log.error("", e);
+        }
+    }
+
+    void processProduct(Page page) {
         Element body = page.getHtml().getDocument().body();
 
         List<Product> products = Lists.newArrayList();
@@ -57,20 +95,13 @@ public class SpiderService implements PageProcessor, Pipeline {
         page.putField("products", products);
     }
 
-    @Override
-    public Site getSite() {
-        return site;
-    }
+    void processCategory(Page page) {
+        log.debug("{}", page);
+        Elements items = page.getHtml().getDocument().body().select("div.items div.clearfix dd a");
+        items.stream().forEach(a -> {
+            String url = a.attr("href");
+            log.debug("{}", url);
+        });
 
-    @Override
-    public void process(ResultItems resultItems, Task task) {
-        List<Product> products = resultItems.get("products");
-
-        List<Product> needSave = products.stream()
-                .filter(product -> !productRepository.exists(product.getId()))
-                .collect(Collectors.toList());
-
-        productRepository.save(needSave);
-        log.info("save {}, total {}", needSave.size(), productRepository.count());
     }
 }
